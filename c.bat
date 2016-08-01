273ccbf4d182d9d5e6b6fb798806a6a50a8cce7b
Yan <belongscy@163.com>
2016-08-01 23:29:42 +0800

    f

---
+++ c.bat
@@ -0,0 +1,7 @@
+call tsc src/node_modules/dfa/index.ts -t es6 -m commonjs
+call tsc src/node_modules/ll1/index.ts -t es6 -m commonjs
+call tsc src/node_modules/nfa/index.ts -t es6 -m commonjs
+call tsc src/node_modules/parse/index.ts -t es6 -m commonjs
+call tsc src/node_modules/productions/index.ts -t es6 -m commonjs
+call tsc src/node_modules/slr1/index.ts -t es6 -m commonjs
+call tsc src/node_modules/utility/index.ts -t es6 -m commonjs
