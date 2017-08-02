fa27351cd864581d8c9a34cab51e803950c23d4f
Yan Chen <yanche.belongs@gmail.com>
2017-08-01 17:44:13 -0700

    small fix

--- src/productions/index.ts
+++ src/productions/index.ts
@@ -1,5 +1,5 @@
 
-import * as prod from "./production";
+import { Production, ProdSet, Symbol } from "./production";
 import * as _ from "lodash";
 
 let chA = "A".charCodeAt(0);
@@ -9,29 +9,29 @@ function strAsNonTerminal(str: string): boolean {
     return chA <= ch && ch <= chZ;
 };
 
-function createProduction(lstr: string, rstr: string): prod.Production {
+function createProduction(lstr: string, rstr: string): Production {
     lstr = lstr.trim();
     rstr = rstr.trim();
     if (lstr.length === 0) throw new Error("left-hand-side is empty");
     if (lstr.indexOf(" ") >= 0) throw new Error("left-hand-side contains more than one symbol: " + lstr);
     if (!strAsNonTerminal(lstr)) throw new Error("left-hand-side is not a non-terminal symbol: " + lstr);
-    if (_.startsWith(lstr, prod.ProdSet.preservedNonTerminalPrefix)) throw new Error("non-terminal cannot start with preserved prefix: " + lstr);
-    let lsymbol = new prod.Symbol(false, lstr);
+    if (_.startsWith(lstr, ProdSet.preservedNonTerminalPrefix)) throw new Error("non-terminal cannot start with preserved prefix: " + lstr);
+    let lsymbol = new Symbol(false, lstr);
     let rsymbols = rstr.split(" ").filter(str => str.length > 0).map(str => {
-        if (_.startsWith(str, prod.ProdSet.preservedNonTerminalPrefix)) throw new Error("non-terminal cannot start with preserved prefix: " + str);
-        return new prod.Symbol(!strAsNonTerminal(str), str);
+        if (_.startsWith(str, ProdSet.preservedNonTerminalPrefix)) throw new Error("non-terminal cannot start with preserved prefix: " + str);
+        return new Symbol(!strAsNonTerminal(str), str);
     });
-    return new prod.Production(lsymbol, rsymbols);
+    return new Production(lsymbol, rsymbols);
 }
 
-// function createProduction2(prodstr: string): prod.Production {
+// function createProduction2(prodstr: string): Production {
 //     let idxarrow = prodstr.indexOf("->");
 //     if (idxarrow < 0) throw new Error("production must has a -> :" + prodstr);
 //     let lstr = prodstr.slice(0, idxarrow), rstr = prodstr.slice(idxarrow + 2);
 //     return createProduction(lstr, rstr);
 // }
 
-function createMultipleProductions(mprodstr: string, splitter?: string): Array<prod.Production> {
+function createMultipleProductions(mprodstr: string, splitter?: string): Array<Production> {
     let idxarrow = mprodstr.indexOf("->");
     if (idxarrow < 0) throw new Error("production must has a -> :" + mprodstr);
     let lstr = mprodstr.slice(0, idxarrow).trim();
@@ -44,20 +44,20 @@ function createMultipleProductions(mprodstr: string, splitter?: string): Array<p
     }
 }
 
-export function createProdSet(mprodarr: Iterable<string>): prod.ProdSet {
-    let prods: Array<prod.Production> = [];
+export function createProdSet(mprodarr: Iterable<string>): ProdSet {
+    let prods: Array<Production> = [];
     for (let mprod of mprodarr) {
         for (let p of createMultipleProductions(mprod)) prods.push(p);
     }
-    return new prod.ProdSet(prods);
+    return new ProdSet(prods);
 }
 
-export function createProdSetWithSplitter(mprodarr: Iterable<string>, splitter?: string): prod.ProdSet {
-    let prods: Array<prod.Production> = [];
+export function createProdSetWithSplitter(mprodarr: Iterable<string>, splitter?: string): ProdSet {
+    let prods: Array<Production> = [];
     for (let mprod of mprodarr) {
         for (let p of createMultipleProductions(mprod, splitter || "|")) prods.push(p);
     }
-    return new prod.ProdSet(prods);
+    return new ProdSet(prods);
 }
 
 export { ProdSet, Symbol, Production, ProductionRef } from "./production";
