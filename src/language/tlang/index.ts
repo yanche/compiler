63b2b8911fd955243f4e37766a721127e5417ea1
Yan Chen <yanche.belongs@gmail.com>
2017-12-21 16:31:38 -0800

    more general compile fn

--- src/language/tlang/index.ts
+++ src/language/tlang/index.ts
@@ -1,7 +1,7 @@
 
 import lex from "./lex";
 import { astConverter, parser, prodSet } from "./syntax";
-import { CompileReturn } from "../../compile";
+import { CompileReturn, CompileError } from "../../compile";
 import { semanticAnalysize, buildGlobalTypes } from "./semantic";
 import { ASTNode_globaldefs } from "./ast";
 import { file } from "../../utility";
@@ -9,33 +9,54 @@ import { generateIntermediateCode } from "./intermediatecode";
 import { generateMIPSCode } from "./mipscode";
 import { ClassLookup, FunctionLookup } from "./util";
 
+export enum CompileOutputFlag {
+    IntermediateCode = 1,
+    ICAfterRegAlloc = 2,
+    MIPS = 4,
+}
 
-function compile(input: string, optimizedicpath: string, icregallocpath: string, mipspath: string): Promise<CompileReturn> {
-    return Promise.resolve().then(() => {
-        let lexret = lex(input, prodSet);
-        if (!lexret.accept) return new CompileReturn(lexret.error);
-        let parseret = parser.parse(lexret.tokens);
-        if (!parseret.accept) return new CompileReturn(parseret.error);
-        let ast = <ASTNode_globaldefs>astConverter.toAST(parseret.root);
-        let gret = buildGlobalTypes(ast);
-        if (!gret.result.accept) return new CompileReturn(gret.result.error);
-        let classlookup = gret.classlookup;
-        let fnlookup = gret.fnlookup;
-        let tret = semanticAnalysize(classlookup, fnlookup);
-        if (!tret.accept) return new CompileReturn(tret.error);
-        let code = generateIntermediateCode(classlookup, fnlookup);
-        let iccode = code.toString();
-        let mips = generateMIPSCode(code, classlookup, fnlookup.mainfnmipslabel);
-        let mipscode = mips.toString();
-        let icafterregallo = code.toString();
-        return Promise.all([file.writeFile(optimizedicpath, iccode), file.writeFile(icregallocpath, icafterregallo), file.writeFile(mipspath, mipscode)]).then(x => new CompileReturn())
-        // return file.writeFile(icpath, code.toString())
-        //     .then(() => file.writeFile(optimizedicpath, code.optimzie().toString()))
-        //     .then(() => file.writeFile(mipspath, code.toMIPS(classlookup).toString()))
-        //     .then(() => new CompileReturn(true));
-    });
+export function compile(input: string, flag: CompileOutputFlag): {
+    intermediateCode?: string;
+    icAfterRegAlloc?: string;
+    mips?: string;
+    error?: CompileError;
+} {
+    let lexret = lex(input, prodSet);
+    if (!lexret.accept) return { error: lexret.error };
+    let parseret = parser.parse(lexret.tokens);
+    if (!parseret.accept) return { error: parseret.error };
+    let ast = <ASTNode_globaldefs>astConverter.toAST(parseret.root);
+    let gret = buildGlobalTypes(ast);
+    if (!gret.result.accept) return { error: gret.result.error };
+    let classlookup = gret.classlookup;
+    let fnlookup = gret.fnlookup;
+    let tret = semanticAnalysize(classlookup, fnlookup);
+    if (!tret.accept) return { error: tret.error };
+    let code = generateIntermediateCode(classlookup, fnlookup);
+    let iccode = flag & CompileOutputFlag.IntermediateCode ? code.toString() : "";
+    let mips = generateMIPSCode(code, classlookup, fnlookup.mainfnmipslabel);
+    let mipscode = flag & CompileOutputFlag.MIPS ? mips.toString() : "";
+    let icafterregallo = flag & CompileOutputFlag.ICAfterRegAlloc ? code.toString() : "";
+    return {
+        intermediateCode: iccode,
+        icAfterRegAlloc: icafterregallo,
+        mips: mipscode
+    };
 }
 
 export function compileFromFile(srcfilepath: string): Promise<CompileReturn> {
-    return file.readFile(srcfilepath).then((data: Buffer) => compile(data.toString("utf8"), srcfilepath + ".optimized.ic", srcfilepath + ".optimized.regallocated.ic", srcfilepath + ".asm"));
+    return file.readFile(srcfilepath)
+        .then((data: Buffer) => {
+            const compret = compile(data.toString("utf8"), CompileOutputFlag.ICAfterRegAlloc | CompileOutputFlag.IntermediateCode | CompileOutputFlag.MIPS);
+            if (compret.error) {
+                return new CompileReturn(compret.error);
+            } else {
+                return Promise.all([
+                    file.writeFile(srcfilepath + ".optimized.ic", compret.intermediateCode),
+                    file.writeFile(srcfilepath + ".optimized.regallocated.ic", compret.icAfterRegAlloc),
+                    file.writeFile(srcfilepath + ".asm", compret.mips)
+                ])
+                    .then(() => new CompileReturn());
+            }
+        });
 }
