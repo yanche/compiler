5e7ea6fa7d14065828b46fddd48567c0f5018a67
Yan Chen <yanche.belongs@gmail.com>
2018-04-15 16:36:27 -0700

    iterable project util

--- src/utility/collection/iterable.test.ts
+++ src/utility/collection/iterable.test.ts
@@ -1,6 +1,6 @@
 
 import * as assert from "assert";
-import { where, empty } from "./iterable";
+import { where, empty, project } from "./iterable";
 import { arrayEqual } from "../../testutil";
 import { initArray } from "./array";
 
@@ -8,6 +8,11 @@ describe("where test", () => {
     it("test case 1", () => {
         const arr = Array.from(where([1, 2, 3, 4, 5], n => n % 2 === 0));
         assert.ok(arrayEqual(arr, [2, 4]));
+    });
+
+    it("index in predicate fn is correct", () => {
+        const arr = Array.from(where([100, 101, 102, 103, 104], (n, i) => n === i + 100));
+        assert.ok(arrayEqual(arr, [100, 101, 102, 103, 104]));
     });
 
     it("iterable from where can be iterated twice", () => {
@@ -33,6 +38,43 @@ describe("where test", () => {
         assert.strictEqual(n2.value, 4);
         assert.strictEqual(arrayEqual(accessed.slice(0, 4), initArray(4, true)), true);
         assert.strictEqual(arrayEqual(accessed.slice(4), initArray(1, false)), true);
+    });
+});
+
+describe("project test", () => {
+    it("test case 1", () => {
+        const arr = Array.from(project([1, 2, 3, 4, 5], n => n + 100));
+        assert.ok(arrayEqual(arr, [101, 102, 103, 104, 105]));
+    });
+
+    it("index in predicate fn is correct", () => {
+        const arr = Array.from(project([100, 101, 102, 103, 104], (n, i) => n + i * 2));
+        assert.ok(arrayEqual(arr, [100, 103, 106, 109, 112]));
+    });
+
+    it("iterable from project can be iterated twice", () => {
+        const arr = [1, 2, 3, 4, 5];
+        const iter = project(arr, n => n + 1);
+        assert.ok(arrayEqual(Array.from(iter), [2, 3, 4, 5, 6]));
+        // iterable from project can be iterated twice
+        assert.ok(arrayEqual(Array.from(iter), [2, 3, 4, 5, 6]));
+    });
+
+    it("no access until usage", () => {
+        const accessed = initArray(5, false);
+        const arr = project([1, 2, 3, 4, 5], (n, i) => {
+            accessed[i] = true;
+            return n + 1;
+        });
+        const iterator = arr[Symbol.iterator]();
+        const n1 = iterator.next();
+        assert.strictEqual(n1.value, 2);
+        assert.strictEqual(arrayEqual(accessed.slice(0, 1), initArray(1, true)), true);
+        assert.strictEqual(arrayEqual(accessed.slice(1), initArray(4, false)), true);
+        const n2 = iterator.next();
+        assert.strictEqual(n2.value, 3);
+        assert.strictEqual(arrayEqual(accessed.slice(0, 2), initArray(2, true)), true);
+        assert.strictEqual(arrayEqual(accessed.slice(2), initArray(3, false)), true);
     });
 });
 
--- src/utility/collection/iterable.ts
+++ src/utility/collection/iterable.ts
@@ -3,6 +3,25 @@ export function* arrayIterator<T>(arr: ReadonlyArray<T>): Iterable<T> {
     yield* arr;
 }
 
+export function where<T>(iter: Iterable<T>, predicate: (item: T, index: number) => boolean): Iterable<T> {
+    return {
+        [Symbol.iterator]: () => whereGenerator(iter, predicate)
+    };
+}
+
+export function project<T, R>(iter: Iterable<T>, projectFn: (item: T, index: number) => R): Iterable<R> {
+    return {
+        [Symbol.iterator]: () => projectGenerator(iter, projectFn)
+    };
+}
+
+export function empty(iter: Iterable<any>): boolean {
+    for (const item of iter) {
+        return false;
+    }
+    return true;
+}
+
 function* whereGenerator<T>(iter: Iterable<T>, predicate: (item: T, index: number) => boolean) {
     let i = 0;
     for (const item of iter) {
@@ -12,15 +31,9 @@ function* whereGenerator<T>(iter: Iterable<T>, predicate: (item: T, index: numbe
     }
 }
 
-export function where<T>(iter: Iterable<T>, predicate: (item: T, index: number) => boolean): Iterable<T> {
-    return {
-        [Symbol.iterator]: () => whereGenerator(iter, predicate)
-    };
-}
-
-export function empty(iter: Iterable<any>): boolean {
+function* projectGenerator<T, R>(iter: Iterable<T>, projectFn: (item: T, index: number) => R) {
+    let i = 0;
     for (const item of iter) {
-        return false;
+        yield projectFn(item, i++);
     }
-    return true;
 }
